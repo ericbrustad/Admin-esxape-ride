9a1891572f9e9695334ffa466a9c6a231a17012c
ericbrustad <62385316+ericbrustad@users.noreply.github.com>
2025-10-01 15:03:20 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 // pages/index.jsx
 export async function getServerSideProps() {
-  const variant = process.env.NEXT_PUBLIC_ADMIN_VARIANT === 'v2' ? '/index02' : '/index01';
-  return { redirect: { destination: variant, permanent: false } };
+  const dest = process.env.NEXT_PUBLIC_ADMIN_VARIANT === 'v2' ? '/index02' : '/index01';
+  return { redirect: { destination: dest, permanent: false } };
 }
 export default function Home(){ return null; }
