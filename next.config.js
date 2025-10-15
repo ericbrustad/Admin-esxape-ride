865ce8a6a064189e2ea844488c8b61ce41d18e74
ericbrustad <62385316+ericbrustad@users.noreply.github.com>
2025-10-15 17:53:40 -0500

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-const rawGameEnabled = process.env.GAME_ENABLED ?? process.env.NEXT_PUBLIC_GAME_ENABLED ?? '0';
-
 const nextConfig = {
   reactStrictMode: false,
   poweredByHeader: false,
-  env: {
-    GAME_ENABLED: rawGameEnabled,
-    NEXT_PUBLIC_GAME_ENABLED: rawGameEnabled,
-  },
 };
 
 module.exports = nextConfig;
