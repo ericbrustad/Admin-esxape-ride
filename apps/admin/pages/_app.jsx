fd10f7d2503582e195b2067662286bdf999e6c87
ericbrustad <62385316+ericbrustad@users.noreply.github.com>
2025-10-28 21:33:08 -0500

    Refactor settings dropdown into global menu

--- apps/admin/pages/_app.jsx
+++ apps/admin/pages/_app.jsx
@@ -1,5 +1,11 @@
 import '../styles/globals.css';
+import AdminSettingsRoot from '../components/AdminSettingsRoot';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <AdminSettingsRoot />
+      <Component {...pageProps} />
+    </>
+  );
 }
